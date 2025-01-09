b763dca34060bcc9e1bebbe0f82d451893fd1ec5
KirGorbunov <KirGorbunov@gmail.com>
2025-01-09 21:22:29 +0300

    Add inputs for number of requests and total time

--- attack.py
+++ attack.py
@@ -8,8 +8,21 @@ logger = setup_logger(__name__, "request.log", level=logging.INFO)
 
 url = "http://127.0.0.1:8000/DDos_test"
 
-number_of_requests = 15
-interval = 1 / number_of_requests
+def get_positive_int(prompt: str) -> int:
+    while True:
+        try:
+            value = int(input(prompt))
+            if value <= 0:
+                raise ValueError
+            return value
+        except ValueError:
+            print("Ошибка: Введите корректное целое положительное число.")
+
+
+number_of_requests = get_positive_int("Введите необходимое количество запросов: ")
+total_time = get_positive_int("Введите время за которое необходимо отправить запросы (в секундах): ")
+
+interval = total_time / number_of_requests
 
 async def send_request(session: aiohttp.ClientSession) -> None:
     try:
@@ -25,6 +38,7 @@ async def send_request(session: aiohttp.ClientSession) -> None:
 async def main() -> None:
     async with aiohttp.ClientSession() as session:
         for i in range(number_of_requests):
+            print(i)
             start_time = time.monotonic()
             await send_request(session)
             elapsed_time = time.monotonic() - start_time
